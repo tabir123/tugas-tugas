760b17f273e281cf705ad28d2d1ffc50b43a2e7a
tabir123 <86651458+tabir123@users.noreply.github.com>
2021-07-23 13:16:01 +0700

    Add files via upload

---
+++ ganjil_genap.js
@@ -0,0 +1,8 @@
+for(var angka = 1; angka <= 10; angka++){
+  if((angka%2)==1){
+    document.write( angka + ' : gajil' + '<br>');
+  }
+  else if ((angka%2)===0) {
+    document.write( angka+' : genap' + '<br>');
+  }
+}
